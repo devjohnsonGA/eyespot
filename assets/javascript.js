91d7c57dc052165f029088ae5e28c31180f92730
devjohnsonGA <dseth.johnson@gmail.com>
2018-06-26 20:26:22 -0400

    changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,36 @@
+// $(document).ready(function(){
+
 function blastoff(breakdown){
     var breakdown = document.getElementById("textSpace").value;
     var breakUp = breakdown.split(" ")
-
-    document.getElementById("words").innerHTML = breakUp[0]
-
-    setTimeout(function(){  
-        for(i=1; i<breakUp.length;i++)(
-    document.getElementById("words").innerHTML = breakUp[i]), 2000   
-    })
+    var delay = 1000
+    var i = 0
+    function timeout() {
+    setTimeout(function () {
+     }, delay);
+} 
+for(; i<breakUp.length;i++) {    
+    document.getElementById("words").innerHTML = breakUp[i]
 }
-    
+}
 
+// })
+
+
+
+// // $(document).ready(function(){
+
+//     function blastoff(breakdown){
+//         var breakdown = document.getElementById("textSpace").value;
+//         var breakUp = breakdown.split(" ")
+//         var delay = 1000
+//         var twinTO = timeout()
+    
+//         function timeout() {
+//         setTimeout(function () {
+//         for(i=0; i<breakUp.length;i++)
+//         document.getElementById("words").innerHTML = breakUp[i];
+//         }, delay + 3000 );
+//     } timeout()
+//     }
+//     // })
